dd77dd5fa38c2b3224e3e165f07a4e0ed6dfb7d0
Leif Johansson <leifjsunet.se>
2024-02-24 21:51:25 +0100

    modern async web

--- src/main.cpp
+++ src/main.cpp
@@ -2,63 +2,77 @@
 #include <WebServer.h>
 #include <ArduinoJson.h>
 #include <WiFiManager.h>
+#include <ESPAsyncWebServer.h>
+#include <SPIFFS.h>
 
-#define TUNED_PIN 10
-#define HOLD_PIN 11
-#define RESET_PIN 12
+
+#define PWR_PIN 25
+#define TUNED_PIN 33
+#define HOLD_PIN 21
+#define RESET_PIN 18
 
 bool tuned = false;
 bool hold = false;
-bool reseet = false;
+bool on = false;
 
 // Web server running on port 80
-WebServer server(80);
+//WebServer server(80);
+AsyncWebServer server(80);
 
-StaticJsonDocument<1024> jsonDocument;
-
-char buffer[1024];
-
-void createJson(char *name, float value, char *unit) {  
-  jsonDocument.clear();
-  jsonDocument["name"] = name;
-  jsonDocument["value"] = value;
-  jsonDocument["unit"] = unit;
-  serializeJson(jsonDocument, buffer);  
-}
- 
-void addJsonObject(char *name, float value, char *unit) {
-  JsonObject obj = jsonDocument.createNestedObject();
-  obj["name"] = name;
-  obj["value"] = value;
-  obj["unit"] = unit; 
+void addBool(JsonDocument *doc, String name, bool value) {
+  if (value) {
+    (*doc)[name].set("true");
+  } else {
+    (*doc)[name].set("false");
+  }
 }
 
-
-void getTuned() {
-  Serial.println("Get all values");
-  jsonDocument.clear(); // Clear json buffer
-  addJsonObject("tuned", tuned, "boolean");
-  serializeJson(jsonDocument, buffer);
-  server.send(200, "application/json", buffer);
-}
-
-void handleReset() {
-  digitalWrite(RESET_PIN, HIGH);
-
-  getTuned();
-}
-
-void handleHold() {
-  digitalWrite(HOLD_PIN, HIGH);
-
-  getTuned();
+void getStatus(AsyncWebServerRequest *request) {
+  StaticJsonDocument<1024> doc;
+  Serial.println("Processing request...");
+  tuned = digitalRead(TUNED_PIN);
+  doc.clear(); // Clear json buffer
+  addBool(&doc, String("tuned"), tuned);
+  addBool(&doc, String("on"), on);
+  addBool(&doc, String("hold"),hold);
+  char buffer[1024];
+  serializeJson(doc, buffer);
+  
+  request->send(200, "application/json", buffer);
 }
 
 void setupApi() {
-  server.on("/is_tuned", getTuned);
-  server.on("/reset", HTTP_POST, handleReset);
-  server.on("/hold", HTTP_POST, handleHold);
- 
+  server.on("/api/is_tuned", HTTP_GET, [](AsyncWebServerRequest *request){
+    getStatus(request);
+  });
+  server.on("/",HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", String());
+  });
+  server.on("/style.css",HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/style.css", String());
+  });
+  server.on("/api/reset", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(RESET_PIN, HIGH);
+    sleep(1);
+    digitalWrite(RESET_PIN, LOW);
+    hold = false;
+    getStatus(request);
+  });
+  server.on("/api/hold", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(HOLD_PIN, HIGH);
+    hold = true;
+    getStatus(request);
+  });
+  server.on("/api/on", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(PWR_PIN, HIGH);
+    on = true;
+    getStatus(request);
+  });
+  server.on("/api/off", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(PWR_PIN, LOW);
+    on = false;
+    getStatus(request);
+  });
   // start server
   server.begin();
 }
@@ -100,15 +114,19 @@ void setup() {
       Serial.println("Connected...yeey :)");
   }
 
+  if(!SPIFFS.begin(true)){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+
   setupApi();
 
+  pinMode(PWR_PIN, OUTPUT);
   pinMode(RESET_PIN, OUTPUT);
   pinMode(HOLD_PIN, OUTPUT);
-  pinMode(TUNED_PIN, INPUT_PULLDOWN);
+  pinMode(TUNED_PIN, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  server.handleClient();
 
 }
